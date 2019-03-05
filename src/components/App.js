331d756fa7548d5c47ab44648fff4878f3951d69
Tom Bowden <tom.bowden@gmail.com>
2019-03-05 15:48:53 +0900

    Modified api endpoint

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import { LIST_TODOS } from '../graphql/queries'
 import { CREATE_TODO, DELETE_TODO, UPDATE_TODO } from '../graphql/mutations'
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://todoapp-server.herokuapp.com/' }),
+  link: new HttpLink({ uri: 'https://todo-serverful-api.herokuapp.com/' }),
   cache: new InMemoryCache(),
 })
 
